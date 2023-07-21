b4eb2dcb0b8ecda46e6c85c9043b15264e2296ee
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2023-07-20 22:10:38 -0400

    Consolidated Scripts into single feature

--- Palette_Reducer.lua
+++ Palette_Reducer.lua
@@ -2,7 +2,7 @@ numColors = 1
 colors = {}
 
 function updateColorPickerUI ()
-    dlg:close()
+    if dlg then dlg:close() end
     
     dlg = getDialogue()
 end
@@ -34,7 +34,12 @@ function applyPaletteReduction ()
                                     for i = 1, numColors do
                                         currColor = dlg.data["clr"..i]
                                         if currDelta == nil then 
-                                            currDelta = calcColorDelta(pixelValue, currColor)
+                                            if dlg.data["colorType"] == "LAB" then
+                                                currDelta = calcColorDeltaLAB(pixelValue, currColor)
+                                            else 
+                                                currDelta = calcColorDeltaRGB(pixelValue, currColor)
+                                            end
+
                                             selectedColor = currColor
                                         else
                                             compare = calcColorDelta(pixelValue, currColor)
@@ -74,6 +79,13 @@ function getDialogue ()
         }
     end
     
+    dlg:combobox{ 
+        id="colorType",
+        label="colorType",
+        option="LAB",
+        options={ "LAB", "RGB" }, 
+    }
+
     dlg:button{
         id = "apply",
         text = "Apply",
@@ -115,7 +127,70 @@ function getDialogue ()
     return dlg
 end
 
-function calcColorDelta(pixel, color)
+
+function RGBtoXYZ(rgb)
+    local r = rgb.red / 255
+    local g = rgb.green / 255
+    local b = rgb.blue / 255
+
+    -- Applying gamma correction
+    r = (r > 0.04045) and (( (r + 0.055) / 1.055) ^ 2.4) or (r / 12.92)
+    g = (g > 0.04045) and (( (g + 0.055) / 1.055) ^ 2.4) or (g / 12.92)
+    b = (b > 0.04045) and (( (b + 0.055) / 1.055) ^ 2.4) or (b / 12.92)
+
+    -- sRGB D65 illuminant
+    local x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
+    local y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
+    local z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
+
+    return x, y, z
+end
+
+-- Helper function to convert XYZ to LAB
+function XYZtoLab(x, y, z)
+    local epsilon = 0.008856
+    local kappa = 903.3
+    local xn, yn, zn = 0.950456, 1.0, 1.088754
+
+    local fX = (x > epsilon) and (x ^ (1 / 3)) or ((kappa * x + 16) / 116)
+    local fY = (y > epsilon) and (y ^ (1 / 3)) or ((kappa * y + 16) / 116)
+    local fZ = (z > epsilon) and (z ^ (1 / 3)) or ((kappa * z + 16) / 116)
+
+    local L = (116 * fY) - 16
+    local a = 500 * (fX - fY)
+    local b = 200 * (fY - fZ)
+
+    return L, a, b
+end
+
+-- Main function to convert RGB to Lab
+function RGBtoLab(rgb)
+    local x, y, z = RGBtoXYZ(rgb)
+    return XYZtoLab(x, y, z)
+end
+
+-- Function to calculate LAB delta
+function calcColorDeltaLAB(pixel, color)
+    -- Convert pixel color to RGB
+    local pixelColor = Color(
+        app.pixelColor.rgbaR(pixel),
+        app.pixelColor.rgbaG(pixel),
+        app.pixelColor.rgbaB(pixel)
+    )
+
+    local pixelL, pixelA, pixelB = RGBtoLab(pixelColor)
+    local targetL, targetA, targetB = RGBtoLab(color)
+
+    local deltaL = math.abs(targetL - pixelL)
+    local deltaA = math.abs(targetA - pixelA)
+    local deltaB = math.abs(targetB - pixelB)
+
+    local totalDelta = math.sqrt(deltaL^2 + deltaA^2 + deltaB^2)
+
+    return totalDelta
+end
+
+function calcColorDeltaRGB(pixel, color)
     delta = math.abs(app.pixelColor.rgbaR(pixel) - color.red)
     + math.abs(app.pixelColor.rgbaB(pixel) - color.blue)
     + math.abs(app.pixelColor.rgbaG(pixel) - color.green)
